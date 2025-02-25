c7925000fd72b36a476d917ad8c1a04fdfecd3e3
Barhon Moshe Dasa <moshebdasa@gmail.com>
2025-02-25 07:28:39 +0200

    sdasd

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,41 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { logError, logInfo } from "./utils/logger";
 import { agentRoute } from "./routes/agent";
 
 const server = fastify();
 
-async function startServer() {
-  try {
-    logInfo("Registering routes...");
+server.addHook(
+  "onRequest",
+  (request: FastifyRequest, reply: FastifyReply, done) => {
+    reply.header("Access-Control-Allow-Origin", "*");
+    reply.header("Access-Control-Allow-Credentials", "true");
+    reply.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
+    reply.header("Access-Control-Expose-Headers", "Content-Length");
+    reply.header(
+      "Access-Control-Allow-Headers",
+      "Accept, Authorization, x-auth-token, Content-Type, X-Requested-With, Range"
+    );
 
-    // רישום המסלולים בצורה בטוחה
-    await server.register(agentRoute);
-    logInfo("agent route registered successfully.");
-
-    // הפעלת השרת
-    await server.listen({ port: 3002, host: "0.0.0.0" });
-    logInfo(`Server running at http://localhost:3002`);
-  } catch (error) {
-    logError("Failed to start server:", { error });
-    process.exit(1);
+    if (request.method === "OPTIONS") {
+      reply.code(200).send();
+    } else {
+      done();
+    }
   }
-}
+);
 
-startServer();
+// טיפול בשגיאות כלליות
+process.on("uncaughtException", (err) => {
+  console.error("uncaughtException:", err.name, err);
+  logError("uncaughtException:" + err.name, err);
+});
+
+logInfo("Registering routes...");
+
+// רישום המסלולים בצורה בטוחה
+server.register(agentRoute);
+logInfo("agent route registered successfully.");
+
+// הפעלת השרת
+server.listen({ port: 3002, host: "0.0.0.0" });
+logInfo(`Server running at http://localhost:3002`);
